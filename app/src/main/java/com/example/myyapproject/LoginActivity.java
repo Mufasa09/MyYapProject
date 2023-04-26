a7d7c3d6bf4c741e2bacd5892da73c90299c9ce0
Joel Musita <jmusita@advsol.com>
2023-04-25 21:35:45 -0500

    Add sign in page/ login info

---
+++ app/src/main/java/com/example/myyapproject/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.myyapproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.ui.AppBarConfiguration;
+
+import com.example.myyapproject.databinding.ActivityMainBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
