4edad77c30b84ef702a08751e34db3575060e56e
Joel Musita <jmusita@advsol.com>
2023-04-26 20:58:49 -0500

    add implementation for google one tap sign on

--- app/src/main/java/com/example/myyapproject/LoginActivity.java
+++ app/src/main/java/com/example/myyapproject/LoginActivity.java
@@ -1,19 +1,42 @@
 package com.example.myyapproject;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.IntentSenderRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.BeginSignInResult;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class LoginActivity extends AppCompatActivity {
 
 
     Button createNewAccountBtn;
     Button loginBtn;
+    ImageView googleBtn;
+    SignInClient oneTapClient;
+    BeginSignInRequest signUpRequest;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +47,8 @@ public class LoginActivity extends AppCompatActivity {
         loginBtn = findViewById(R.id.SignInButton);
         TextView username = (TextView) findViewById(R.id.username);
         TextView password = (TextView) findViewById(R.id.password);
+        googleBtn = findViewById(R.id.googleImage);
+
 
         createNewAccountBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,5 +70,69 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+
+        //region Google Sign on
+
+
+        oneTapClient = Identity.getSignInClient(this);
+        signUpRequest = BeginSignInRequest.builder()
+                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        // Your server's client ID, not your Android client ID.
+                        .setServerClientId(getString(R.string.web_client_id))
+                        // Show all accounts on the device.
+                        .setFilterByAuthorizedAccounts(false)
+                        .build())
+                .build();
+
+        ActivityResultLauncher<IntentSenderRequest> activityResultLauncher =
+                registerForActivityResult(new ActivityResultContracts.StartIntentSenderForResult(),
+                        new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if(result.getResultCode()== Activity.RESULT_OK)
+                        {
+                            try {
+                                SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(result.getData());
+                                String idToken = credential.getGoogleIdToken();
+                                if (idToken !=  null) {
+                                    String email = credential.getId();
+                                    Toast.makeText(getApplicationContext(),"Email: " +email, Toast.LENGTH_SHORT).show();
+
+                                }
+                            } catch (ApiException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                oneTapClient.beginSignIn(signUpRequest)
+                        .addOnSuccessListener(LoginActivity.this,
+                                new OnSuccessListener<BeginSignInResult>() {
+                            @Override
+                            public void onSuccess(BeginSignInResult result) {
+                                //
+                                IntentSenderRequest intentSenderRequest = new IntentSenderRequest.Builder(result.getPendingIntent().
+                                        getIntentSender()).
+                                        build();
+                                activityResultLauncher.launch(intentSenderRequest);
+                            }
+                        })
+                        .addOnFailureListener(LoginActivity.this,
+                                new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d("TAG", e.getLocalizedMessage());
+                            }
+                        });
+            }
+        });
+        //endregion
     }
+
+
 }
