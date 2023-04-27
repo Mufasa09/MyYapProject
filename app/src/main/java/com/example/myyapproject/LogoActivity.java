6af8b2b3d293c6adb51f73dad87673588fc2ad7a
Joel Musita <jmusita@advsol.com>
2023-04-26 22:16:28 -0500

    wrap up image loading screen

---
+++ app/src/main/java/com/example/myyapproject/LogoActivity.java
@@ -0,0 +1,29 @@
+package com.example.myyapproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LogoActivity extends AppCompatActivity {
+
+    ImageView logo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+        logo = findViewById(R.id.imageView);
+        NavigateToLoginPage();
+    }
+
+    public void NavigateToLoginPage(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(LogoActivity.this, LoginActivity.class);
+                startActivity(intent);            }
+        }, 1000);
+    }
+}
